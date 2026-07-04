f2e075e785b1c6ccec60f52b604ca88f1189f355
shiva-m-kr <itsshiva1724@gmail.com>
2026-07-04 11:25:15 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -87,8 +87,8 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "lax",
-            secure: false,
+            sameSite: "None",
+            secure: true,
         })
         return res.status(201).json(user)
 
