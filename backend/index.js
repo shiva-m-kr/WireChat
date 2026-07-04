a8a103355ebc6a7e32f00d138de97d0dacc1f2fa
shiva-m-kr <itsshiva1724@gmail.com>
2026-07-04 09:54:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://wirechat-f.onrender.com",
     credentials:true
 }))
 
